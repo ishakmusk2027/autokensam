a272a2c278651c5b891f1992f758ca85c289f273
TEST Ball <160318630+ishakmusk2027@users.noreply.github.com>
2024-03-22 15:00:48 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bot:  {
+    owners: ["1204876472708235270","1204876472708235270"],  // ايدي الاونر
+    botID: "1220398320761962556",  // Bot ID
+    GuildId: "1220368525160353875",   // ايدي السيرفير
+    ClientId: "1220398320761962556",    // ايدي البوت
+    serverinvte: "https://discord.gg/eetAMuGnnb", // انفايت سيرفر
+    clientSECRET: process.env.client, // سكريت
+    callbackURL: "https://autokensam.onrender.com/login", // الكال باك
+    inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1220398320761962556&permissions=8&redirect_uri=https%3A%2F%2Fautokensam.onrender.com%2Flogin&scope=bot", // انفايت البوت
+    TheLinkVerfy: 'https://discord.com/oauth2/authorize?client_id=1220398320761962556&response_type=code&redirect_uri=https%3A%2F%2Fautokensam.onrender.com%2Flogin&scope=guilds+guilds.join+identify', // رابط اوثو رايز بالصلاحيه ادخال الي سيرفرات
+    prefix: 'a', 
+    ceatogry: '1220375173778112612', // كاتوجري الي يفتح فيها تكت شراء
+    TOKEN: (process.env.midd), // توكن 
+    Price: 2000,   // سعر العضو الواحد
+    TraId: '1204876472708235270' // ايدي الي يتحوله كريديت
+  },
+  website: {
+    PORT: "3001",
+  }
+}
